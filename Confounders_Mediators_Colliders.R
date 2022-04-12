ef5f54f88746533931e67efd15bf64d19f61ca34
Benjamin Moran <ben.moran@health.nsw.gov.au>
2022-04-12 15:02:22 +1000

    Add collider

--- Confounders_Mediators_Colliders.R
+++ Confounders_Mediators_Colliders.R
@@ -16,14 +16,25 @@ ggdag_classic(confounder_dag) + theme_dag_blank()
 
 mediator_dag <- dagify(
   Outcome ~ Exposure,
-  Confounder ~ Exposure,
-  Outcome ~ Confounder,
+  Mediator ~ Exposure,
+  Outcome ~ Mediator,
   coords = list(
-    x = c(Exposure = 0, Confounder = 1, Outcome = 2),
-    y = c(Exposure = 0, Confounder = 1.5, Outcome = 0)
+    x = c(Exposure = 0, Mediator = 1, Outcome = 2),
+    y = c(Exposure = 0, Mediator = 1.5, Outcome = 0)
   )
 )
 
 ggdag_classic(mediator_dag) + theme_dag_blank() 
 
+collider_dag <- dagify(
+  Outcome ~ Exposure,
+  Collider ~ Exposure,
+  Collider ~ Outcome,
+  coords = list(
+    x = c(Exposure = 0, Collider = 1, Outcome = 2),
+    y = c(Exposure = 0, Collider = 1.5, Outcome = 0)
+  )
+)
+
+ggdag_classic(collider_dag) + theme_dag_blank() 
 
